6b4377b78cbd07604fa56e7c95a8d93fbf218628
fmsilva1996 <fmsilva1996@gmail.com>
2020-06-27 15:57:15 +0000

    feat: add basic resolvers for login and register

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -5,8 +5,24 @@ import {
   CurrencyResolver,
   PositiveIntResolver,
 } from 'graphql-scalars';
+import User from './models/user.model';
 
 export const resolvers = {
+  Query: {
+    async login(_: any, args: any) {
+      const user = await User.findOne({ email: args.email });
+      if (!user) return 'no user';
+      //TODO: verify pw
+      return user;
+    },
+  },
+  Mutation: {
+    async register(_: any, args: any) {
+      const user = await User.create(args);
+      return user;
+    },
+  },
+
   DateTime: DateTimeResolver,
   EmailAddress: EmailAddressResolver,
   PhoneNumber: PhoneNumberResolver,
