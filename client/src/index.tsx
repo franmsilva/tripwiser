9c192f32765af916a72c52c88575d5020c016a8e
Andrew Cooke <cookecodes89@gmail.com>
2020-06-27 09:56:36 +0200

    feat: added apollo cache/client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,10 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import cache from './GraphQl/cache';
+import { ApolloClient, HttpLink, ApolloProvider } from '@apollo/client';
+import { BrowserRouter } from 'react-router-dom';
+
+const link = new HttpLink({
+  uri: 'http://localhost:4000/'
+})
+
+const client = new ApolloClient({
+  cache,
+  link
+})
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client} >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
