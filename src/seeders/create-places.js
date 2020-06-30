f3fcaecf1ed1df0fcf89d62e360a4de82f693945
fmsilva1996 <fmsilva1996@gmail.com>
2020-06-30 09:39:02 +0000

    feat: added function to seed database with available places

---
+++ src/seeders/create-places.js
@@ -0,0 +1,29 @@
+const Place = require('../models/place.model.js');
+const skyPlaces = require('./places.json');
+
+const seedDB = (places) => {
+  // eslint-disable-next-line array-callback-return
+  places.Continents.map((continent) => {
+    // eslint-disable-next-line array-callback-return
+    continent.Countries.map((country) => {
+      // eslint-disable-next-line array-callback-return
+      country.Cities.map((city) => {
+        city.Airports.map(async (airport) => {
+          await Place.create({
+            cityId: airport.CityId,
+            cityName: city.Name,
+            countryId: airport.CountryId,
+            countryName: country.Name,
+            location: airport.Location,
+            airportId: airport.Id,
+            aiportName: airport.Name,
+          });
+        });
+      });
+    });
+  });
+  // eslint-disable-next-line no-console
+  console.log('All places added to database 🧳');
+};
+
+seedDB(skyPlaces);
